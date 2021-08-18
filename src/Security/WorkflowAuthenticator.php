ee0030137b9a2d989544c3a28b66e36c2d2a93f8
Aaron Den Hond <aaron.denhond@gmail.com>
2021-08-18 11:05:56 +0200

    routing for login

--- src/Security/WorkflowAuthenticator.php
+++ src/Security/WorkflowAuthenticator.php
@@ -51,7 +51,7 @@ class WorkflowAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('ticket_index'));
+        return new RedirectResponse($this->urlGenerator->generate('log_issue'));
         //REROUTE TO /TICKET UPON SUCCESFULL LOGIN
         //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
         
